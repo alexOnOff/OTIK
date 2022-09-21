7419ed7ca81312b042e7cf1645db6e8f1ad17dde
Alexey <Alexey@DESKTOP-PE694DS>
2022-09-21 11:21:02 +0300

    Добавьте файлы проекта.

---
+++ OTIK-Lab1/FileManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTIK_Lab1
+{
+    public class FileManager
+    {
+        private List<string> FilePath { get; set; }
+
+        public FileManager()
+        {
+            FilePath = new List<string>();
+        }
+
+        public FileManager(List<string> filePath)
+        {
+            this.FilePath = filePath;
+        }
+
+        public void ReadFiles()
+        {
+            foreach (var path in this.FilePath)
+            {
+                Console.WriteLine(path);
+
+                FileInfo fileInfo = new FileInfo(path);
+                Console.WriteLine(File.ReadAllText(path));
+
+            }
+        }
+    }
+}
---
+++ OTIK-Lab1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace OTIK_Lab1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            List<string> FilePath = new List<string> { "C:\\Work\\Study\\OTIKDir\\example.txt", "C:\\Work\\Study\\OTIKDir\\pic.bmp" };
+            FileManager fileManager = new FileManager(FilePath);
+            fileManager.ReadFiles();
+        }
+    }
+}
