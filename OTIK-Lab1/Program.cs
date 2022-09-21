70ad4f8200d22a62da16771e58c74ffe30bffb31
Alexey Kartashev <68597810+alexOnOff@users.noreply.github.com>
2022-09-21 11:27:34 +0300

    Init Program

--- OTIK-Lab1/Program.cs
+++ OTIK-Lab1/Program.cs
@@ -6,7 +6,7 @@ namespace OTIK_Lab1
     {
         public static void Main()
         {
-            List<string> FilePath = new List<string> { "C:\\Work\\Study\\OTIKDir\\example.txt", "C:\\Work\\Study\\OTIKDir\\pic.bmp" };
+            List<string> FilePath = new List<string> { "C:\\Work\\Study\\OTIKDir\\example.txt" };
             FileManager fileManager = new FileManager(FilePath);
             fileManager.ReadFiles();
         }
