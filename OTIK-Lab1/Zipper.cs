9c9e1cdcb02d63ca0deb92708d8b4a7ae147f295
Alexey Kartashev <68597810+alexOnOff@users.noreply.github.com>
2022-09-21 12:11:24 +0300

    Add zipper class

---
+++ OTIK-Lab1/Zipper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTIK_Lab1
+{
+    public class Zipper
+    {
+        private static byte[] _signature = new byte[] { 0x6e , 0x6b, 0x76, 0x64 };
+
+
+
+    }
+}
