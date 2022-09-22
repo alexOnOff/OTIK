9854c4e754be204cd3703c4dea6b14604b950d06
Kirill Dolbilov <dolbilov.kirill@gmail.com>
2022-09-22 16:44:56 +0300

    feat: update code structure

--- OTIK-Lab1/FileManager.cs
+++ OTIK-Lab1/FileManager.cs
@@ -1,42 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace OTIK_Lab1;
 
-namespace OTIK_Lab1
+public class FileManager
 {
-    public class FileManager
+    private readonly List<string> _filePaths = new();// массив путей до файлов
+    public List<FileInfo> FileInfos = new(); // массив "информации" файлов
+    public string PathToZip;     // тут понятно
+
+    public FileManager(List<string> filePath, string pathToZip)
     {
-        public List<string> FilePath;// массив путей до файлов
-        public List<FileInfo> Files; // массив "информации" файлов
-        public string PathToZip;     // тут понятно
+        _filePaths = filePath;
+        PathToZip = pathToZip;
+    }
 
-        public FileManager()
+    public void ReadFiles()
+    {
+        //перебираю все путя и добавляю в массив fileInfo все что есть
+        foreach (var path in _filePaths)
         {
-            FilePath = new List<string>();
-            PathToZip = string.Empty;
-            Files = new List<FileInfo>();
-        }
+            FileInfo fileInfo = new FileInfo(path);
 
-        public FileManager(List<string> filePath, string pathToZip)
-        {
-            FilePath = filePath;
-            PathToZip = pathToZip;
-            Files = new List<FileInfo>();
-        }
-
-        public void ReadFiles()
-        {
-            //перебираю все путя и добавляю в массив fileInfo все что есть
-            foreach (var path in this.FilePath)
+            if(fileInfo.Exists)
             {
-                FileInfo fileInfo = new FileInfo(path);
-
-                if(fileInfo.Exists)
-                {
-                    Files.Add(fileInfo);
-                }
+                FileInfos.Add(fileInfo);
             }
         }
     }
--- OTIK-Lab1/Program.cs
+++ OTIK-Lab1/Program.cs
@@ -1,24 +1,21 @@
-﻿
+﻿namespace OTIK_Lab1;
 
-namespace OTIK_Lab1
+public static class Program
 {
-    public class Program
+    public static void Main()
     {
-        public static void Main()
-        {
-            // сначала даем на вход файлики, которые надо будет добавить в архив, тут у меня их два
-            List<string> FilePath = new List<string> { "D:\\Study\\ОТИК\\example.txt" , "D:\\Study\\ОТИК\\pic.bmp" };
+        // сначала даем на вход файлики, которые надо будет добавить в архив, тут у меня их два
+        List<string> FilePath = new List<string> { @"D:\Study\ОТИК\example.txt" , @"D:\Study\ОТИК\pic.bmp" };
 
-            // дальше указываю папку, где создать архив
-            string pathToZip = "D:\\Study\\ОТИК\\";
+        // дальше указываю папку, где создать архив
+        string pathToZip = @"D:\Study\ОТИК\";
 
-            // создаю файловый менеджер, который в себе хранит все загруженные файлы
-            FileManager fileManager = new FileManager(FilePath, pathToZip);
-            //fileManager.ReadFiles(); // считываю загруженные файлы
+        // создаю файловый менеджер, который в себе хранит все загруженные файлы
+        FileManager fileManager = new FileManager(FilePath, pathToZip);
+        //fileManager.ReadFiles(); // считываю загруженные файлы
 
-            // добавляю их в архив
-            //Zipper.Encode(fileManager);
-            Zipper.Decode(fileManager);
-        }
+        // добавляю их в архив
+        //Zipper.Encode(fileManager);
+        Zipper.Decode(fileManager);
     }
 }
